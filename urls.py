6eb8c94390a8648bb3d0efac0e01664071ed4447
egor.merkushkin <egor.merkushkin@redsolution.ru>
2021-04-09 12:40:31 +0500

    update on urls

--- urls.py
+++ urls.py
@@ -6,5 +6,7 @@ app_name = 'test_module'
 
 urlpatterns = [
     url(r'^$', views.ModuleInfoView.as_view(), name='info'),
+    url(r'^dashboard/subitem/$', views.ModuleMenuSubItemView.as_view(), name='dashboard-subitem'),
+    url(r'^users/subitem/$', views.ModuleMenuSubItemView.as_view(), name='users-subitem'),
     url(r'^create/test/', views.ModuleMenuSubItemView.as_view(), name='create-test'),
 ]
