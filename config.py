6b9bccd101c55ad4ee725b6b6d1123b9879f0575
whisper <into.the.dissonance@gmail.com>
2021-04-12 02:03:30 +0500

    configs generation

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
 from typing import Optional
 from django.template.loader import render_to_string
-from .models import TestPluginConfig
+from .models import TestPluginConfig as PluginConfigModel
+from .apps import TestPluginConfig as PluginConfig
 
 
-def get_module_config(config_instance) -> Optional[str]:
-    if not isinstance(config_instance, TestPluginConfig):
+def get_module_config() -> Optional[str]:
+    configs = PluginConfigModel.objects.all()
+    config_global = configs.filter(virtual_host="global")
+    configs_for_hosts = configs.exclude(virtual_host="global")
+    if not configs.exists():
         return None
-    config_template = 'test_plugin/config.yml'
+    config_template = "%s/config.yml" % PluginConfig.name
     context = {
-        "module_name": "test_plugin",
-        'some_key': "key",
-        "some_value": config_instance.some_value
+        'module_name': PluginConfig.name,
+        'config_global': config_global,
+        'configs_for_hosts': configs_for_hosts
     }
     return render_to_string(template_name=config_template, context=context, request=None)
--- models.py
+++ models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from modules_installation.models import BasePluginConfig
+
+
+class TestPluginConfig(BasePluginConfig):
+    some_value = models.CharField(verbose_name="Some value", default="value")
+
+    class Meta:
+        managed = True
 
 
 class TestModel1(models.Model):
